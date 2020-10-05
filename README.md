591f692dc809796961d729f30c2cd70ebf2fb66e
sokorn01 <65432990+sokorn01@users.noreply.github.com>
2020-10-05 14:52:52 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# ten11-next
+### Build in development
+> npm run dev
+
+### Build in production
+> npm run build
+> npm run start
+
+### Install svn
+> brew install svn
+
+### install svn in visual studio code
+> go to exstention svn
+
+### Checkout project
+> svn checkout url
+
+### configure redux with project next.js
+1. instatll package
+    > npm i --save redux react-redux next-redux-wrapper redux-thunk
---
+++ next.config.js
@@ -0,0 +1,26 @@
+const withCSS = require("@zeit/next-css");
+require("dotenv").config();
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+const withImages = require("next-images");
+
+module.exports = withCSS(
+  withImages({
+    distDir: "build",
+    inlineImageLimit: 16384,
+    webpack(config, options) {
+      config.plugins = config.plugins || [];
+      config.plugins = [
+        ...config.plugins,
+
+        // Read the .env file
+        new Dotenv({
+          path: path.join(__dirname, ".env"),
+          systemvars: true,
+        }),
+      ];
+      return config;
+    },
+  })
+);
