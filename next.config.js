53b4eb4b9ef2f7d9e49eb46d67f1de51c8b46918
sokorn01 <65432990+sokorn01@users.noreply.github.com>
2020-10-20 10:33:07 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,32 @@
 const withCSS = require("@zeit/next-css");
-require("dotenv").config();
-const path = require("path");
-const Dotenv = require("dotenv-webpack");
+// require("dotenv").config();
+// const path = require("path");
+// const Dotenv = require("dotenv-webpack");
 const withImages = require("next-images");
+require('dotenv').config()
+const webpack = require('webpack')
 
 module.exports = withCSS(
   withImages({
     // basePath: "/Test",
     inlineImageLimit: 16384,
-    webpack(config, options) {
-      config.plugins === config.plugins || [];
-      config.plugins === [
-        ...config.plugins,
-        // Read the .env file
-        new Dotenv({
-          path: path.join(__dirname, ".env"),
-          systemvars: true,
-        }),
-      ];
-      return config;
-    },
+    webpack: (config) => {
+      config.plugins.push(
+        new webpack.EnvironmentPlugin(process.env)
+      )
+      return config
+    }
+    // webpack(config, options) {
+    //   config.plugins = config.plugins || [];
+    //   config.plugins = [
+    //     ...config.plugins,
+    //     // Read the .env file
+    //     new Dotenv({
+    //       path: path.join(__dirname, ".env"),
+    //       systemvars: true,
+    //     }),
+    //   ];
+    //   return config;
+    // },
   })
 );
