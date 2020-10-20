2465299ba8b7c4129b6d1ba076694ff6b7f6406d
sokorn01 <65432990+sokorn01@users.noreply.github.com>
2020-10-20 09:25:45 +0700

    account

--- pages/account.js
+++ pages/account.js
@@ -1,20 +1,7 @@
 import React from "react";
-import { serviceApiFormData } from "../shared/hooks/seviceApi";
-
 
 const Account = () => {
   return <div>Login page</div>;
 };
-
-export async function getStaticProps({ params }) {
-  const api = serviceApiFormData();
-  const res = await api.post("MenuApp/getMenu");
-  const menu = res.data;
-  return {
-    props: {
-      menu,
-    },
-  };
-}
 
 export default Account;
